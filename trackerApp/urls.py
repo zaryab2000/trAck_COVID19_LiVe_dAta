d1e16dc512c0da4d15deaadbca5f1502dc3b88ad
zaryab2000 <zaryabafser2000@gmail.com>
2020-04-21 14:44:18 +0530

    added country-based search feature

--- trackerApp/urls.py
+++ trackerApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('data/',views.data, name='data')
+    path('data/',views.data, name='data'),
+    path('country_data/', views.getCountryData, name='country_data')
 ]
--- trackerApp/views.py
+++ trackerApp/views.py
@@ -37,4 +37,47 @@ def data(request):
 
 	
 def getCountryData(request):
-	pass
+	country_url = f'https://covid19.mathdro.id/api/countries'
+	countries_list=[]
+
+	country_data = requests.get(country_url)
+	con_json=country_data.json()
+
+	countries=con_json['countries']
+	# confirmed = json['confirmed']['value']
+	# recover  =json['recovered']['value']
+	# deaths = json['deaths']['value']
+
+	for country in countries:
+		name = country['name']
+		countries_list.append(name)
+
+	if request.method =='POST':
+		name = request.POST.get('country_name')
+		detail_url = f'https://covid19.mathdro.id/api/countries/{name}'
+		
+		country_detail = requests.get(detail_url)
+		detail_json=country_detail.json()
+
+		confirmed = detail_json['confirmed']['value']
+		recover  =detail_json['recovered']['value']
+		deaths = detail_json['deaths']['value']
+
+		print(name)
+
+		context = {
+		'confirmed':confirmed,
+		'recovered':recover,
+		'deaths':deaths,
+		'names':countries_list
+		}
+
+	else:
+		context = {
+		'confirmed':0,
+		'recovered':0,
+		'deaths':0,
+		'names':countries_list
+	}
+
+	return render(request, 'trackerApp/country_data.html',context)
