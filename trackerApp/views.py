e3ded798e9ba56cfde4d79de54a34bb1d79f1838
zaryab2000 <zaryabafser2000@gmail.com>
2020-04-23 01:19:19 +0530

    redced page-load time

--- trackerApp/views.py
+++ trackerApp/views.py
@@ -6,17 +6,7 @@ def index(request):
 	return render(request, 'trackerApp/base.html')
 
 def getCountries():
-	country_url = f'https://covid19.mathdro.id/api/countries'
-	countries_list=[]
-
-	country_data = requests.get(country_url)
-	con_json=country_data.json()
-
-	countries=con_json['countries']
-
-	for country in countries:
-		name = country['name']
-		countries_list.append(name)
+	countries_list=['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Congo (Brazzaville)', 'Congo (Kinshasa)', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Cyprus', 'Czechia', 'Denmark', 'Diamond Princess', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Holy See', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Korea, South', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'MS Zaandam', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Somalia', 'South Africa', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Taiwan*', 'Tanzania', 'Thailand', 'Timor-Leste', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'US', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'West Bank and Gaza', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe']
 
 	return countries_list
 
@@ -77,21 +67,20 @@ def top(request):
 	vals=[]
 	init_list={}
 
-	# cont_list = getCountries()
+	cont_list = getCountries()
 
-	# for name in cont_list:
-	# 	country_url = f'https://covid19.mathdro.id/api/countries/{name}'
-	# 	cont_data = requests.get(country_url)
-	# 	json=cont_data.json()
+	for name in cont_list:
+		country_url = f'https://covid19.mathdro.id/api/countries/{name}'
+		cont_data = requests.get(country_url)
+		json=cont_data.json()
 
-	# 	try:
-	# 		conf = json['confirmed']['value']
-	# 	except KeyError:
-	# 		conf = 0
+		try:
+			conf = json['confirmed']['value']
+		except KeyError:
+			conf = 0
 
-	# 	init_list[name]=conf
-	# new_data = {k: v for k, v in sorted(init_list.items(), key=lambda item: item[1], reverse=True)[:10]}
-	new_data= {'US': 834858, 'Spain': 208389, 'Italy': 187327, 'France': 159315, 'Germany': 149401, 'United Kingdom': 134637, 'Turkey': 98674, 'Iran': 85996, 'China': 83868, 'Russia': 57999}
+		init_list[name]=conf
+	new_data = {k: v for k, v in sorted(init_list.items(), key=lambda item: item[1], reverse=True)[:10]}
 
 	for key,value in new_data.items():
 		countries.append(key)
